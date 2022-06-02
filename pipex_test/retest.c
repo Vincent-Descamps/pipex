58569d28aba7125b67d6d63cdab3a914fd7eed6c
Vince <vincedescamps@gmail.com>
2022-06-02 13:07:06 +0200

    path_ok

--- pipex_test/retest.c
+++ pipex_test/retest.c
@@ -6,19 +6,19 @@
 /*   By: vdescamp <vdescamp@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 11:01:26 by vdescamp          #+#    #+#             */
-/*   Updated: 2022/06/02 11:15:10 by vdescamp         ###   ########.fr       */
+/*   Updated: 2022/06/02 13:06:04 by vdescamp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv, char **envp)
+char	*get_path(char *cmd, char **envp)
 {
 	int		i;
 	int		len;
 	char	*env_path;
 	char	**paths;
-	char	*tmp;
+	char	*cmd_path;
 
 	i = 0;
 	while (envp[i])
@@ -35,12 +35,42 @@ int	main(int argc, char **argv, char **envp)
 	}
 	paths = ft_split(env_path, ':');
 	free (env_path);
-	for(int i = 0; i<6; i++)
-		ft_printf("=> %s\n", paths[i]);
 	i = 0;
 	while (paths[i])
 	{
-
+		paths[i] = ft_strjoin(paths[i], "/");
+		i++;
 	}
+	/*for(int i = 0; i<6; i++)
+		ft_printf("=> %s\n", paths[i]);*/
+	i = 0;
+	while (paths[i])
+	{
+		cmd_path = ft_strjoin(paths[i], cmd);
+		if (access(cmd_path, F_OK && X_OK) == 0)
+			return (cmd_path);
+		free(cmd_path);
+		i++;
+	}
+	return ("error\n");
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*cmd_path;
+	char	*options[3] = {"ls", "-la", NULL};
+	char	*cmd = "ls";
+
+	(void)argc;
+	(void)argv;
+	cmd_path = get_path(cmd, envp);
+	if (!cmd_path)
+	{
+		perror(cmd_path);
+		return (-1);
+	}
+	execve(cmd_path, options, envp);
+	printf("Victory!");
+	free(cmd_path);
 	return (0);
 }
