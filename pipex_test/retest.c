8d3f0d308e461725f2d67234f342108c0f1b0757
Vince <vincedescamps@gmail.com>
2022-06-07 15:08:30 +0200

    main

--- pipex_test/retest.c
+++ pipex_test/retest.c
@@ -6,11 +6,28 @@
 /*   By: vdescamp <vdescamp@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 11:01:26 by vdescamp          #+#    #+#             */
-/*   Updated: 2022/06/02 13:25:33 by vdescamp         ###   ########.fr       */
+/*   Updated: 2022/06/07 12:42:48 by vdescamp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+char	*get_cmd_path(char **paths, char	*cmd)
+{
+	char	*cmd_path;
+	int		i;
+
+	i = 0;
+	while (paths[i])
+	{
+		cmd_path = ft_strjoin(paths[i], cmd);
+		if (access(cmd_path, F_OK && X_OK) == 0)
+			return (cmd_path);
+		free(cmd_path);
+		i++;
+	}
+	return ("**error can't find path...**\n");
+}
 
 char	*get_path(char *cmd, char **envp)
 {
@@ -35,31 +52,17 @@ char	*get_path(char *cmd, char **envp)
 	}
 	paths = ft_split(env_path, ':');
 	free (env_path);
-	i = 0;
-	while (paths[i])
-	{
+	i = -1;
+	while (paths[++i])
 		paths[i] = ft_strjoin(paths[i], "/");
-		i++;
-	}
-	/*for(int i = 0; i<6; i++)
-		ft_printf("=> %s\n", paths[i]);*/
-	i = 0;
-	while (paths[i])
-	{
-		cmd_path = ft_strjoin(paths[i], cmd);
-		if (access(cmd_path, F_OK && X_OK) == 0)
-			return (cmd_path);
-		free(cmd_path);
-		i++;
-	}
-	return ("**error can't find path...**\n");
+	return (get_cmd_path(paths, cmd));
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*cmd_path;
-	char	*options[3] = {"ls", "-la", NULL};
-	char	*cmd = "ls";
+	char	*options[3] = {"ps", "-e", NULL};
+	char	*cmd = "ps";
 
 	(void)argc;
 	(void)argv;
@@ -69,6 +72,7 @@ int	main(int argc, char **argv, char **envp)
 		perror(cmd_path);
 		return (-1);
 	}
+	//system("leaks a.out");
 	execve(cmd_path, options, envp);
 	printf("Victory!");
 	free(cmd_path);
